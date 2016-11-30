baef477c4c78bec5eaad057c9808bd7ae5e2d649
Mathael <LebocPhilippe@live.be>
2016-11-30 11:38:49 +0100

    feat(Entity): add Article

---
+++ src/sil13/VitrineBundle/Entity/Article.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace sil13\VitrineBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Article
+ */
+class Article
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @var bool
+     */
+    private $isSoldOut;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Article
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Article
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set isSoldOut
+     *
+     * @param boolean $isSoldOut
+     * @return Article
+     */
+    public function setIsSoldOut($isSoldOut)
+    {
+        $this->isSoldOut = $isSoldOut;
+
+        return $this;
+    }
+
+    /**
+     * Get isSoldOut
+     *
+     * @return boolean 
+     */
+    public function getIsSoldOut()
+    {
+        return $this->isSoldOut;
+    }
+}
