5630b1c51732ff6e6d787ac0b8833da71e930625
Mathael <LebocPhilippe@live.be>
2016-12-02 11:02:56 +0100

    feat(CRUD): generation of Article and Category entities

---
+++ src/sil13/VitrineBundle/Form/ArticleType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace sil13\VitrineBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')->add('description')->add('isSoldOut')->add('category')->add('user')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'sil13\VitrineBundle\Entity\Article'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'sil13_vitrinebundle_article';
+    }
+
+
+}
